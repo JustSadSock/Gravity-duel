0bcdc7aaed8c23d042165917cc8cfde67c4a317a
JustSadSock <136240492+JustSadSock@users.noreply.github.com>
2025-06-11 02:04:33 +0300

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,1 +1,25 @@
+// audio.js
 
+// Простая обёртка над HTMLAudioElement
+const flipSound = new Audio('assets/sfx/flip.wav');
+flipSound.volume = 0.5;
+
+const scoreSound = new Audio('assets/sfx/score.wav');
+scoreSound.volume = 0.7;
+
+/**
+ * Воспроизводит звук смены гравитации.
+ */
+export function playFlip() {
+  // Чтобы быстро флипать, клонируем элемент
+  const s = flipSound.cloneNode();
+  s.play().catch(() => {/* игнорируем ошибки autoplay */});
+}
+
+/**
+ * Воспроизводит звук гола.
+ */
+export function playScore() {
+  const s = scoreSound.cloneNode();
+  s.play().catch(() => {/* игнорируем ошибки autoplay */});
+}
